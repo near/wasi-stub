979924567f9f2cda09dcc5d398dbcda4761efdd4
Bo Yao <bo@near.org>
2023-02-02 17:01:42 +0800

    fix when script_dir has space

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SYSTEM=$(uname)
 if [ "${SYSTEM}" = "Linux" ]; then
-    export LD_LIBRARY_PATH=${SCRIPT_DIR}/../lib
+    export LD_LIBRARY_PATH="${SCRIPT_DIR}"/../lib
 elif [ "${SYSTEM}" = "Darwin" ]; then
-    export DYLD_LIBRARY_PATH=${SCRIPT_DIR}/../lib
+    export DYLD_LIBRARY_PATH="${SCRIPT_DIR}"/../lib
 else
     echo "Unsupported system ${SYSTEM}"
     exit 1
 fi
-${SCRIPT_DIR}/wasi-stub $@
+"${SCRIPT_DIR}/wasi-stub" $@
